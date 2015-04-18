348eb41154a917ac250f01ffd04fd6455d65f234
Julian Perrott <julian_perrott@hotmail.com>
2015-04-18 21:32:17 +0100

    SquareDigits

---
+++ Various/squareDigits.js
@@ -0,0 +1,33 @@
+﻿function squareDigits(num) {
+
+    if (num < 0) { return -squareDigits(-num); }
+
+    return parseInt(
+        num.toString()
+        .split('')
+        .map(function (digit) { return Math.pow(parseInt(digit), 2); })
+        .join(''));
+}
+
+describe("squareDigits - ", function () {
+
+    //it("-1", function () {
+    //    expect(squareDigits(-1)).toEqual(-1);
+    //});
+
+    //it("1", function () {
+    //    expect(squareDigits(1)).toEqual(1);
+    //});
+
+    it("81", function () {
+        expect(squareDigits(81)).toEqual(641);
+    });
+
+    it("9119", function () {
+        expect(squareDigits(9119)).toEqual(811181);
+    });
+
+    it("-9119", function () {
+        expect(squareDigits(-9119)).toEqual(-811181);
+    });
+});

4969b4df41e0e4dbaa29ca77474eb0f794a0a41c
Julian Perrott <julian_perrott@hotmail.com>
2015-05-23 17:54:36 +0100

    Feymans squares

---
+++ FSharp/2 kyu/EscapeTheMines.fs
@@ -0,0 +1,133 @@
+﻿namespace FSharp.CodeWars
+
+(* http://www.codewars.com/kata/5326ef17b7320ee2e00001df/train/haskell (2 kyu)
+
+A poor miner is trapped in a mine and you have to help him to get out !
+
+Only, the mine is all dark so you have to tell him where to go.
+
+In this kata, you will have to implement a method solve(map, miner, exit) that has to return the path the miner must take to reach the exit as an array of moves, 
+such as : ['up', 'down', 'right', 'left']. 
+There are 4 possible moves, up, down, left and right, no diagonal.
+
+map is a 2-dimensional array of boolean values, representing squares. false for walls, true for open squares (where the miner can walk). 
+It will never be larger than 5 x 5. It is laid out as an array of columns. All columns will always be the same size, though not necessarily 
+the same size as rows (in other words, maps can be rectangular). The map will never contain any loop, so there will always be only one possible path. 
+The map may contain dead-ends though.
+
+miner is the position of the miner at the start, as an object made of two zero-based integer properties, x and y. For example {x:0, y:0} would 
+be the top-left corner.
+
+exit is the position of the exit, in the same format as miner.
+
+Note that the miner can't go outside the map, as it is a tunnel.
+
+Let's take a pretty basic example :
+
+let map = [[True, False],
+           [True, True]]
+
+solve map (0,0) (1,1)
+-- Should return [R, D]
+*)
+
+module EscapeTheMines = 
+    let verticalSlice ar = 
+        [| for i in 0..8 -> Array.map (fun (x : int []) -> x.[i]) ar |]
+    
+    type XY = int * int
+    type Move = U | D | R | L
+
+
+    //solve :: [[Bool]] -> XY -> XY -> [Move] 
+    let solve (grid : bool[][]) (miner : XY) (exit : XY) = [U]
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit.MsTest
+open NHamcrest.Core
+open EscapeTheMines
+open System
+
+[<TestClass>]
+type EscapeTheMines_tests() = 
+    
+//    [<TestMethod>]
+//    member test.``Array is finished``() = doneOrNot finished |> should equal "Finished!"
+//    
+//    [<TestMethod>]
+//    member test.``Array is not finished``() = doneOrNot notFinished |> should equal "Try again!"
+//
+//
+//  describe "A trivial map (1x1)" $ do
+//    let map = [[True]];
+//    it "Should return an empty list, since we're already at the goal" $ do
+//      solve map (0,0) (0,0) `shouldBe` []
+//      
+//  describe "A pretty simple map (2x2)" $ do
+//    let map = unmap [" #"
+//                    ,"  "
+//                    ]
+//    it "Should return the only correct move" $ do
+//      solve map (0,0) (1,0) `shouldBe` [R]
+//      
+//    it "Should return the only moves necessary" $ do
+//      solve map (0,0) (1,1) `shouldBe` [R, D]
+//      
+//      
+//  describe "A linear map(1x4)" $ do
+//    let map = unmap [" "
+//                    ," "
+//                    ," "
+//                    ," "
+//                    ]
+//
+//    it "Should return a chain of moves to the right" $ do
+//      solve map (0,0) (3,0) `shouldBe` [R, R, R]
+//
+//    it "Should return a chain of moves to the left" $ do
+//      solve map (3,0) (0,0) `shouldBe` [L, L, L]
+//
+//  describe "Should walk around an obstacle (3x3 map)" $ do
+//    let map = unmap ["   "
+//                    ,"## "
+//                    ,"   "
+//                    ]
+//
+//    it "Should return the right sequence of moves" $ do
+//      solve map (0,0) (2,0) `shouldBe` [D, D, R, R, U, U]
+//
+//  describe "Should be able to change directions multiple times (5x5 map)" $ do
+//    let map = unmap ["  ###"
+//                    ,"#  ##"
+//                    ,"##  #"
+//                    ,"###  "
+//                    ,"#### "
+//                    ]
+//
+//    it "Should return a step sequence of moves" $ do
+//      solve map (0,0) (4,4) `shouldBe` [D, R, D, R, D, R, D, R]
+//
+//  describe "Should avoid dead-ends (5x5 map)" $ do
+//    let map = unmap ["   ##"
+//                    ,"## # "
+//                    ,"     "
+//                    ," # ##"
+//                    ,"#    "
+//                    ]
+//
+//    it "Should return the right moves" $ do
+//      solve map (0,0) (4,4) `shouldBe` [D, D, R, R, R, R, D, D]
+//      
+//unmap = map (map (== ' '))    
+
+
+ let xx =["   ##"
+                    ;"## # "
+                    ;"     "
+                    ;" # ##"
+                    ;"#    "
+                    ]
+
+let toTF xs = Seq.map (fun x-> x='#') xs
+
+xx |> List.map (fun x->toTF x)
---
+++ FSharp/6 kyu/Feynman's square question.fs
@@ -0,0 +1,50 @@
+﻿namespace FSharp.CodeWars
+(* http://www.codewars.com/kata/551186edce486caa61000f5c/train/haskell (6 kyu)
+Feynman's squares
+
+Richard Phillips Feynman was a well-known American physicist and a recipient of the Nobel Prize in Physics. He worked in theoretical physics and pioneered the field of quantum computing.
+
+Recently, an old farmer found some papers and notes that are believed to have belonged to Feynman. Among notes about mesons and electromagnetism, there was a napkin where he wrote a simple puzzle: "how many different squares are there in a grid of NxN squares?".
+
+For example, when N=2, the answer is 5: the 2x2 square itself, plus the four 1x1 squares in its corners:
+
+(see disatoba-feynman.gif)
+
+Task
+
+You have to write a function
+
+countSquares :: Integer -> Integer
+that solves Feynman's question in general. The input to your function will always be a positive integer.
+
+Examples
+
+countSquares 1 =  1
+countSquares 2 =  5
+countSquares 3 = 14
+(Adapted from the Sphere Online Judge problem SAMER08F by Diego Satoba)
+*)
+
+module Feynman_s_square_question =
+
+    let countSquares n = [1..n] |> List.map (fun x -> x*x) |> List.sum
+        
+
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit.MsTest
+open NHamcrest.Core
+open Feynman_s_square_question
+open System
+
+[<TestClass>] 
+type ``Feynman_s_square_question tests`` ()=
+
+    [<TestMethod>] member test.``Feynman_s_square 1``  ()= countSquares 1 |> should equal 1
+    [<TestMethod>] member test.``Feynman_s_square 2``  ()= countSquares 2 |> should equal 5
+    [<TestMethod>] member test.``Feynman_s_square 3``  ()= countSquares 3 |> should equal 14
+    [<TestMethod>] member test.``Feynman_s_square 5``  ()= countSquares 5 |> should equal 55
+    [<TestMethod>] member test.``Feynman_s_square 8``  ()= countSquares 8 |> should equal 204
+    [<TestMethod>] member test.``Feynman_s_square 15`` ()= countSquares 15 |> should equal 1240
+
+   
